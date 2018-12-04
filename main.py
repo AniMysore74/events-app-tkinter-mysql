f6b396ba41d574e2ce98bba4c671fa54ac658c0f
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2018-12-05 01:02:30 +0530

    Lift up style settings, add and delete talks

--- main.py
+++ main.py
@@ -7,24 +7,62 @@ from PIL import ImageTk, Image
 # connect to db
 db= _mysql.connect(host="localhost",user="root",passwd="ArkAngel",db="EVENTS")
 
+
+btnConfig = { 
+    'background': 'white',
+    'height': 2,
+    'width': 20,
+    'font': ('Arial', 13)
+}
+
+lblConfig = { 
+    'bg' : 'white',
+    'font': ('Helvetica', 15),
+    'pady': 15
+}
+
+headerLbl = {
+    'bg' : 'white',
+    'font': ('Helvetica', 20, 'bold underline'),
+    'pady': 20,
+    'bd': 1,
+}
+
+entryCfg = {
+    'font': ('Helvetica', 15),
+} 
+
+optionsConfig = {
+    'background': 'white',
+    'font': ('Helvetica', 15),
+}
+# Superclass that all other pages derive from
 class Page(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, background="white", *args, **kwargs)
+    def __init__(self,  *args, **kwargs):
+        tk.Frame.__init__(self, background="white", *args)
+        
+        # add ICACCI logo to top of page if (logo='pack') or (logo='grid')  is passed in constructor
+        # use pack or grid accordingly
+        if 'logo' in kwargs:
+            image = Image.open('logo.png')
+            image = image.resize((1024,280), Image.ANTIALIAS)
+            photo = ImageTk.PhotoImage(image)
+            img = tk.Label(self,image=photo)
+            img.image = photo
+            if kwargs['logo'] == 'pack':
+                img.pack(pady=(0,40))
+            if kwargs['logo'] == 'grid':
+                img.grid(row=0,column=0,columnspan=20)
+            
     def show(self):
         self.lift()
 
 class SchedulePage(Page):
     def __init__(self, controller, *args, **kwargs):
         self.controller = controller
-        Page.__init__(self, *args, **kwargs)
+        Page.__init__(self, *args, logo='grid', **kwargs)
 
-        tk.Frame(master=self, width=1024, height=20, bg="#1769AA").grid(column=0, columnspan=20)
-
-        intro = tk.Frame(master=self, width=1024, height=60, bg="#2196f3")
-        intro.grid(column=0, columnspan=20)
-
-        title = tk.Label(self, font=("-*-Courier-*-R0200-*-*-*-*-ISO8859-1", 20), background="#2196f3", foreground="white",text="International Conference on \nAdvances in Computing, Communications and Informatics".upper())
-        title.grid(column=1, row=1, columnspan=20)
+        tk.Frame(master=self, width=1024, height=30, bg="white").grid(column=0, columnspan=20)
 
         query = 'select * from Event NATURAL JOIN Location'
         db.query(query)
@@ -37,17 +75,21 @@ class SchedulePage(Page):
         header[0].grid(sticky="W", column=startcol, row=9)
         header.append(tk.Label(self, background="white", text="Location", font=("-*-Times-Bold-R-*--*-250-*-*-*-*-ISO8859-1", 20)))
         header[1].grid(sticky="W", column=(startcol+1), row=9)
+        header.append(tk.Label(self, background="white", text="Time", font=("-*-Times-Bold-R-*--*-250-*-*-*-*-ISO8859-1", 20)))
+        header[2].grid(sticky="W", column=(startcol+2), row=9)
 
+        backButton = tk.Button(self, text="Back", **btnConfig, command = lambda :self.controller.showFrame('MenuPage'))
+        backButton.grid(row=20,column=10)
         eventLbl = []
         placeLbl = []
         timeLbl = []
         btns = []
         for i,event in enumerate(events):
-            eventLbl.append(tk.Label(self, background="white", text=event['Name'],font=("-*-Times-Bold-R-*--*-200-*-*-*-*-ISO8859-1", 20)))
+            eventLbl.append(tk.Label(self, text=event['Name'], **lblConfig))
             eventLbl[i].grid(sticky="W", column=startcol, row=i+10)
-            placeLbl.append(tk.Label(self, background="white", text=event['PlaceName'],))
+            placeLbl.append(tk.Label(self, text=event['PlaceName'],**lblConfig))
             placeLbl[i].grid(sticky="W", column=(startcol+1), row=i+10)
-            timeLbl.append(tk.Label(self, background="white", text=event['StartTime']))
+            timeLbl.append(tk.Label(self, text=event['StartTime'], **lblConfig))
             timeLbl[i].grid(sticky="W", column=(startcol+2), row=i+10)
             e = event['EventId']
             btns.append(tk.Button(self, background="white", text="Info", command=lambda e=e: self.go_to_page(e) ))
@@ -62,54 +104,78 @@ class SchedulePage(Page):
 class TalksPage(Page):
     def __init__(self, controller, *args, **kwargs):
         self.controller = controller
-        Page.__init__(self, *args, **kwargs)
-        talkLbl= []
-        eventNamelbl = tk.Label()
-        backButton = tk.Button(self, background="white", text="Back", command=lambda: controller.showFrame('SchedulePage') )
-        backButton.grid(column=1, row=10)
+        Page.__init__(self, *args, logo='grid',**kwargs)
+        self.cleared = True
+        tk.Frame(master=self, width=1024, height=30, bg="white").grid(column=0, columnspan=20)
+
+        backButton = tk.Button(self, **btnConfig, text="Back", command=lambda: controller.showFrame('SchedulePage') )
+        backButton.grid(column=9, row=20)
+
+    def clear(self):
+        if not self.cleared:
+            self.eventNamelbl.destroy()
+            self.inchargeName.destroy()
+            self.inchargeContact.destroy()
+            self.place.destroy()
+            for column in [self.talkLbl, self.spkrLbl, self.timeLbl]:
+                for row in column:
+                    row.destroy()
 
     def putData(self):
-        query = 'select * from Event where EventId = ' + self.controller.EventId 
+        self.clear()
+        self.cleared = False
+        query = 'select * from Location NATURAL JOIN Event NATURAL JOIN Incharge where EventId = ' + self.controller.EventId 
         db.query(query)
         r = db.store_result()
         event = list(r.fetch_row(maxrows=0,how=1))
-        self.eventNamelbl = tk.Label(self,font=("Aial", 20),background="white", text=('      '+event[0]['Name'].decode()+'     '))
-        self.eventNamelbl.grid(column = 0, row = 1)
+        self.eventNamelbl = tk.Label(self,font=("Arial", 20), background="white", text=('Session: '+event[0]['Name'].decode()+''))
+        self.eventNamelbl.grid(column = 4, row = 2, pady=(15,0), columnspan=10)
+
+        self.inchargeName = tk.Label(self,font=('Arial',15), background="white", text=('Incharge: '+event[0]['InchargeName'].decode()+'')) 
+        self.inchargeName.grid(column=4, row=3, columnspan=10)
+
+        self.inchargeContact = tk.Label(self,font=('Arial',15), background="white", text=('Contact No: '+event[0]['ContactNo']+'')) 
+        self.inchargeContact.grid(column=4, row=4, columnspan=10)
+
+        self.place = tk.Label(self,font=('Arial',15), background="white", text=('Venue : '+event[0]['PlaceName'].decode()+'')) 
+        self.place.grid(column=4, row=5,  pady=(0,20), columnspan=10)
 
         query = 'select * from Speaker NATURAL JOIN Talk where EventId = ' + self.controller.EventId 
         db.query(query)
         r = db.store_result()
         talks = list(r.fetch_row(maxrows=0,how=1))
 
+        self.header = []
+        startcol = 8
+        self.header.append(tk.Label(self, text="Talk Title", **headerLbl))
+        self.header[0].grid(sticky="W", column=startcol, row=6)
+        self.header.append(tk.Label(self,text="Speaker", **headerLbl))
+        self.header[1].grid(sticky="W", column=(startcol+1), row=6)
+        self.header.append(tk.Label(self, text="Time", **headerLbl))
+        self.header[2].grid(sticky="W", column=(startcol+2), row=6)
+
         self.talkLbl = []
+        self.spkrLbl = []
+        self.timeLbl = []
         for i,talk in enumerate(talks):
-            self.talkLbl.append(tk.Label(self,background="white", text=' '+talk['Title'].decode()+' '))
-            self.talkLbl[i].grid(column=10, row=i)
-        
+            self.talkLbl.append(tk.Label(self, text=' '+talk['Title'].decode()+' ', **lblConfig))
+            self.talkLbl[i].grid(column=startcol, row=i+8,
+             sticky='W')
+            self.spkrLbl.append(tk.Label(self, text=' '+talk['SpeakerName'].decode()+' ', **lblConfig))
+            self.spkrLbl[i].grid(column=startcol+1, row=i+8, sticky='W')
+            self.timeLbl.append(tk.Label(self, text=' '+talk['StartTime']+' ', **lblConfig))
+            self.timeLbl[i].grid(column=startcol+2, row=i+8, sticky='W')
+
 class MenuPage(Page):
     def __init__(self, controller, *args, **kwargs):
         self.controller = controller
-        Page.__init__(self, *args, **kwargs)
-
-        btnConfig = { 
-            'background': 'white',
-            'height': 2,
-            'width': 20,
-            'font': ('Arial', 13)
-        }
-
-        image = Image.open('logo.png')
-        image = image.resize((1024,280), Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-        img = tk.Label(self,image=photo)
-        img.image = photo
-        img.pack(pady=(0,40))
+        Page.__init__(self,logo='pack', *args, **kwargs)
 
         controls = {}
         controls['SchedulePage'] = tk.Button(self,text="See Schedule",**btnConfig)
-        controls['LoginPage'] = tk.Button(self, text="Login as Administrator",**btnConfig)
         controls['MapPage'] = tk.Button(self, text="View Map",**btnConfig)
         controls['SponsorsPage'] = tk.Button(self, text="Conference Sponsors",**btnConfig)
+        controls['LoginPage'] = tk.Button(self, text="Login as Administrator",**btnConfig)
 
         for i,control in enumerate(controls):
             controls[control].config(command=lambda page=control: self.controller.showFrame(page))
@@ -120,70 +186,54 @@ class MapPage(Page):
         self.controller = controller
         Page.__init__(self, *args, **kwargs)
 
-        btnConfig = { 
-            'background': 'white',
-            'height': 2,
-            'width': 20,
-            'font': ('Arial', 13)
-        }
-
         image = Image.open('map.png')
         photo = ImageTk.PhotoImage(image)
-        img = tk.Label(self,image=photo)
+        img = tk.Label(self, bg='white', image=photo)
         img.image = photo
-        img.pack(fill='both')
+        img.pack(fill='both',expand=1)
 
+        backButton = tk.Button(self, text="Back", **btnConfig, command = lambda :self.controller.showFrame('MenuPage'))
+        backButton.pack()
 
 class SponsorsPage(Page):
     def __init__(self, controller, *args, **kwargs):
         self.controller = controller
-        Page.__init__(self, *args, **kwargs)
-
-        btnConfig = { 
-            'background': 'white',
-            'height': 2,
-            'width': 20,
-            'font': ('Arial', 13)
-        }
+        Page.__init__(self, *args, logo='grid', **kwargs)
+        
         query = 'select * from Sponsor'
         db.query(query)
         r = db.store_result()
+
+        tk.Label(self, text='Sponsor Name', **headerLbl).grid(row=1, column='9', sticky='W')
+        tk.Label(self, text='Role', **headerLbl).grid(row=1, column='11', sticky='W')
+        
         sponsors = list(r.fetch_row(maxrows=0,how=1))
         for i,sponsor in enumerate(sponsors):
-            tk.Label(self, text=sponsor['SponsorName']).grid(row=i+2, column=2)
-            tk.Label(self, text=sponsor['Role']).grid(row=i+2, column=3)
-            
+            tk.Label(self, text=sponsor['SponsorName'], **lblConfig).grid(row=i+2, column='9', sticky='W')
+            tk.Label(self, text=sponsor['Role'], **lblConfig).grid(row=i+2, column='11', sticky='W')
+        
+        backButton = tk.Button(self, text="Back", **btnConfig, command = lambda :self.controller.showFrame('MenuPage'))
+        backButton.grid(row=20,column=10)
 
 class LoginPage(Page):
     def __init__(self, controller, *args, **kwargs):
         self.controller = controller
-        Page.__init__(self, *args, **kwargs)
+        Page.__init__(self, *args, logo='grid', **kwargs)
 
-        btnConfig = { 
-            'background': 'white',
-            'height': 2,
-            'width': 20,
-            'font': ('Arial', 13)
-        }
-
-        image = Image.open('logo.png')
-        image = image.resize((1024,280), Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-        img = tk.Label(self,image=photo)
-        img.image = photo
-        img.pack(pady=(0,40))
-
-        L1 = tk.Label(self,text="User Name")
-        L1.pack()
-        self.Username = tk.Entry(self,bd =5)
-        self.Username.pack()
-        L2 = tk.Label(self,text="Password")
-        L2.pack()
-        self.Password = tk.Entry(self,bd =5)
-        self.Password.pack()
+        L1 = tk.Label(self,text="User Name", **lblConfig)
+        L1.grid(row = 3, column = 8)
+        self.Username = tk.Entry(self, **entryCfg)
+        self.Username.grid(row = 3, column = 10)
+        L2 = tk.Label(self,text="Password", **lblConfig)
+        L2.grid(row = 4, column = 8)
+        self.Password = tk.Entry(self, **entryCfg, show="*")
+        self.Password.grid(row = 4, column = 10)
 
         SubmitButton = tk.Button(self,text='Login', **btnConfig, command=self.login)
-        SubmitButton.pack()
+        SubmitButton.grid(row = 5, column = 8)
+        backButton = tk.Button(self, text="Back", **btnConfig ,command = lambda :self.controller.showFrame('MenuPage'))
+        backButton.grid(row = 5, column = 10)
+
 
     def login(self):
         if(self.Username.get()=='Admin' and self.Password.get()=='Pass'):
@@ -194,25 +244,9 @@ class LoginPage(Page):
 class AdminPage(Page):
     def __init__(self, controller, *args, **kwargs):
         self.controller = controller
-        Page.__init__(self, *args, **kwargs)
-
-        btnConfig = { 
-            'background': 'white',
-            'height': 2,
-            'width': 20,
-            'font': ('Arial', 13)
-        }
-
-        image = Image.open('logo.png')
-        image = image.resize((1024,280), Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-        img = tk.Label(self,image=photo)
-        img.image = photo
-        img.pack(pady=(0,40))
+        Page.__init__(self, *args, logo='pack', **kwargs)
 
         controls = {}
-        controls['AddSpeakerPage'] = tk.Button(self,text="Add new speaker",**btnConfig)
-        controls['DeleteSpeakerPage'] = tk.Button(self, text="Delete a speaker",**btnConfig)
         controls['AddTalkPage'] = tk.Button(self, text="Add a talk",**btnConfig)
         controls['DeleteTalkPage'] = tk.Button(self, text="Delete a talk",**btnConfig)
         controls['MenuPage'] = tk.Button(self, text="Logout",**btnConfig)
@@ -220,6 +254,83 @@ class AdminPage(Page):
         for i,control in enumerate(controls):
             controls[control].config(command=lambda page=control: self.controller.showFrame(page))
             controls[control].pack(fill='y',pady=5)
+
+class AddTalkPage(Page):
+    def __init__(self, controller, *args, **kwargs):
+        self.controller = controller
+        Page.__init__(self, *args, logo='grid', **kwargs)
+        
+        titleLabel = tk.Label(self,text="Talk Title", **lblConfig)
+        titleLabel.grid(row = 3, column = 8)
+        self.title = tk.Entry(self, **entryCfg)
+        self.title.grid(row = 3, column = 10)
+        timeLabel = tk.Label(self,text="Start Time", **lblConfig)
+        timeLabel.grid(row = 4, column = 8)
+        self.time = tk.Entry(self, **entryCfg)
+        self.time.grid(row = 4, column = 10)
+        spkrLabel = tk.Label(self,text="Speaker Name", **lblConfig)
+        spkrLabel.grid(row = 5, column = 8)
+        self.spkr = tk.Entry(self, **entryCfg)
+        self.spkr.grid(row = 5, column = 10)
+        
+        options = [  
+            'Computer Vision and the Internet (VisionNet-18)', 
+            'Natural Language Processing (NLP-18)' , 
+            'Artificial Intelligence',  
+            'Machine Learning/Data Engineering'
+        ]
+
+        optionLabel = tk.Label(self,text="Select Event", **lblConfig)
+        optionLabel.grid(row = 6, column = 8)
+
+        var = tk.StringVar(self)
+        var.set(options[0])
+        
+        option = tk.OptionMenu(self, var, *options)
+        option.grid(row = 6, column = 10)
+        option.config(**optionsConfig)
+
+        SubmitButton = tk.Button(self,text='Add Talks', **btnConfig, command=self.addTalk)
+        SubmitButton.grid(row = 7, column = 8)
+        backButton = tk.Button(self, text="Back", **btnConfig, command = lambda :self.controller.showFrame('AdminPage'))
+        backButton.grid(row = 7, column = 10)
+
+    def addTalk(self):
+        query = 'insert into Speaker(SpeakerName) values("'+self.spkr.get()+'");'
+        db.query(query)
+        query = 'select SpeakerId from Speaker where SpeakerName="'+self.spkr.get()+'";'
+        db.query(query)
+        r = db.store_result()
+        spkrid = int(r.fetch_row(how=1)[0]['SpeakerId'])
+        self.event = 1
+        query = 'insert into Talk(Title,StartTime,SpeakerId,EventId) values('
+        query += '"'+self.title.get()+'",'
+        query += '"'+self.time.get()+'",'
+        query += str(spkrid)+','
+        query += str(self.event)
+        query += ');'
+        db.query(query)
+
+        tk.messagebox.showinfo("Talk added!")
+
+class DeleteTalkPage(Page):
+    def __init__(self, controller, *args, **kwargs):
+        self.controller = controller
+        Page.__init__(self, *args, logo='pack', **kwargs)
+
+        
+
+        SubmitButton = tk.Button(self,text='Login', **btnConfig, command=self.login)
+        SubmitButton.pack()
+        backButton = tk.Button(self, text="Back", command = lambda :self.controller.showFrame('MenuPage'))
+        backButton.pack()
+
+    def login(self):
+        if(self.Username.get()=='Admin' and self.Password.get()=='Pass'):
+            self.controller.showFrame('AdminPage')
+        else:
+            pass
+            tk.messagebox.showinfo("Incorrect Login!", "The username and password you entered are incorrect!")
 
 
 class MainView(tk.Frame):
@@ -234,8 +345,9 @@ class MainView(tk.Frame):
         self.frames['SponsorsPage'] = SponsorsPage(self)
         self.frames['LoginPage'] = LoginPage(self)
         self.frames['AdminPage'] = AdminPage(self)
+        self.frames['AddTalkPage'] = AddTalkPage(self)
+        self.frames['DeleteTalkPage'] = DeleteTalkPage(self)
 
-        buttonframe = tk.Frame(self)
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
 
